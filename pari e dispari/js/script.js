de5f68c8d0a3ea7b4bf34045d3da0ab11c4c3022
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-03 16:37:32 +0200

    dichiarare il vincitore

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,6 +1,6 @@
 // L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-var pariDispari = prompt("Inserisci la tua scelta. Pari o Dispari?");
-console.log(pariDispari);
+var parolaUtente = prompt("Inserisci la tua scelta. Pari o Dispari?");
+console.log(parolaUtente);
 
 var numeroUtente = parseInt(prompt("Inserisci un numero compreso tra 1 e 5!"));
 console.log("numero utente",numeroUtente);
@@ -21,4 +21,23 @@ console.log("somma",somma);
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
+function pariDispari(num) {
+    if (num % 2 == 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+}
+
+var risultato = pariDispari(somma)
+console.log("Il risultato della somma è",risultato);
+
 // Dichiariamo chi ha vinto.
+
+if ((parolaUtente = "pari") && (somma % 2 == 0)) {
+    console.log("Complimenti hai vinto!");
+} else {
+    console.log("mi dispiace hai perso");
+} 
+
+
