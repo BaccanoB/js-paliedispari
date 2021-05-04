63da33cbbc5ac12c7e8036396594c8c107da6a42
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-04 09:29:53 +0200

    Inserite stampe in pagina

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -3,12 +3,14 @@ do {
     var parolaUtente = prompt("Inserisci la tua scelta. Pari o Dispari?");
     console.log(parolaUtente);
 } while (parolaUtente !== "pari" && parolaUtente !== "dispari") ;
+document.getElementById("parola").innerHTML = "Hai scelto " + parolaUtente;
 
 
 do {
     var numeroUtente = parseInt(prompt("Inserisci un numero compreso tra 1 e 5!"));
     console.log("numero utente",numeroUtente);
 } while (isNaN(numeroUtente) || numeroUtente > 5 || numeroUtente < 1);
+document.getElementById("utente").innerHTML = "Il numero che hai scelto è " + numeroUtente;
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
@@ -18,10 +20,12 @@ function numeroRandom(numero) {
 
 var numeroComputer = numeroRandom(numeroUtente);
 console.log("numero computer",numeroComputer);
+document.getElementById("computer").innerHTML = "Il numero scelto dal computer è " + numeroComputer;
 
 // Sommiamo i due numeri
 var somma = numeroUtente + numeroComputer; 
 console.log("somma",somma);
+document.getElementById("somma").innerHTML = "La somma è " + somma;
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
@@ -40,8 +44,10 @@ console.log("Il risultato della somma è",risultato);
 
 if (parolaUtente.toLowerCase() == risultato.toLowerCase()) {
     console.log("Complimenti hai vinto!");
+    document.getElementById("risultato").innerHTML = "Complimenti hai vinto!";
 } else {
     console.log("Mi dispiace hai perso!");
+    document.getElementById("risultato").innerHTML = "Mi dispiace hai perso!";
 }
 
 
